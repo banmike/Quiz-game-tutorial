fe80e634d7477493f5fbe656c05b6de8aa91dcef
Nguyễn Khánh <131245621+enkeiornk@users.noreply.github.com>
2023-11-13 23:09:54 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Thời gian diễn ra truyện Sơn Tinh, Thủy Tinh vào thời đại nào của lịch sử dân tộc?",
+        answer: "A. Thời đại Văn Lang - Âu Lạc",
+        options: [
+            "A. Thời đại Văn Lang - Âu Lạc",
+            "B. Thời nhà Lí",
+            "C. Thời nhà Trần",
+            "D. Thời nhà Nguyễn"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Truyện Sơn Tinh, Thủy Tinh thuộc thể loại nào?",
+        answer: "B. Truyền thuyết",
+        options: [
+            "A. Cổ tích",
+            "B. Truyền thuyết",
+            "C. Sử thi",
+            "D. Tiểu thuyết"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Trong câu “Bão bùng thân bọc lấy thân / Tay ôm tay níu tre gần nhau thêm” là câu nhân hóa tả?",
+        answer: "C. Hoạt động",
+        options: [
+            "A. Hình dáng",
+            "B. Tính chất",
+            "C. Hoạt động",
+            "D. Trạng thái"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Nhân vật chính trong truyện Sơn Tinh, Thủy Tinh là ai?",
+        answer: "C. Sơn Tinh, Thủy Tinh",
+        options: [
+            "A. Sơn Tinh",
+            "B. Thủy Tinh",
+            "C. Sơn Tinh, Thủy Tinh",
+            "D. Mị Nương"
+        ]
+    },
+    {
+        numb: 5,
+        question: "“Dòng sông mới điệu làm sao/ Nắng lên mặc áo lụa đào thướt tha” là câu thơ miêu tả?",
+        answer: "D. Tính cách",
+        options: [
+            "A. Hoạt động",
+            "B. Hình dáng",
+            "C. Tính chất",
+            "D. Tính cách"
+        ]
+    },
+    {
+        numb: 6,
+        question: "An-đéc-xen là nhà văn của nước nào?",
+        answer: "A. Đan Mạch",
+        options: [
+            "A. Đan Mạch",
+            "B. Thụy Sĩ",
+            "C. Pháp",
+            "D. Thụy Điển"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Có mấy kiểu ẩn dụ thường gặp?",
+        answer: "D. Cả ba đáp án trên",
+        options: [
+            "A. Ẩn dụ hình thức, cách thức",
+            "B. Ẩn dụ chuyển đổi cảm giác",
+            "C. Ẩn dụ phẩm chất",
+            "D. Cả ba đáp án trên"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Đâu là vế A trong câu thơ so sánh “Mặt trời xuống biển như hòn lửa”?",
+        answer: "B. Mặt trời xuống biển",
+        options: [
+            "A. Mặt trời",
+            "B. Mặt trời xuống biển",
+            "C. Như",
+            "D. Hòn lửa"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Từ ghép là những từ như thế nào?",
+        answer: "A. Hai từ ghép lại với nhau",
+        options: [
+            "A. Hai từ ghép lại với nhau",
+            "B. Hai từ ghép lại với nhau trong đó có một từ chính và một từ phụ",
+            "C. Hai tiếng trở lên ghép lại với nhau, có quan hệ với nhau về mặt ý nghĩa",
+            "D. Cả ba đáp án trên đều đúng"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Biên bản gồm mấy phần?",
+        answer: "A. 3 phần",
+        options: [
+            "A. 3 phần",
+            "B. 4 phần",
+            "C. 5 phần",
+            "D. 6 phần"
+        ]
+    },
+];
